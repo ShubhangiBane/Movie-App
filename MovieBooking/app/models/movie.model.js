b80df4927f0a6b0f85945b795860bb9063976c42
ShubhangiBane <98038587+ShubhangiBane@users.noreply.github.com>
2022-07-06 18:18:41 +0530

    Completed All Checkpoint

--- MovieBooking/app/models/movie.model.js
+++ MovieBooking/app/models/movie.model.js
@@ -1,49 +1,52 @@
 module.exports = (mongoose) => {
   const Movie = mongoose.model(
     "movie",
-    mongoose.Schema({
-      movieid: { type: Number, required: true },
-      title: { type: String },
-      published: Boolean,
-      released: Boolean,
-      poster_url: {
-        type: String,
-        unique: true,
-        dropDups: true,
-      },
-      release_date: { type: Date },
-      publish_date: { type: Date },
-      artists: [
-        {
-          artistid: Number,
-          first_name: String,
-          last_name: String,
-          wiki_url: String,
-          profile_url: String,
-          movies: [],
-        },
-      ],
-      genres: [String],
-      duration: Number,
-      critic_rating: Number,
-      trailer_url: String,
-      wiki_url: String,
-      story_line: String,
-      shows: [
-        {
-          id: Number,
-          theatre: {
-            name: String,
-            city: String,
+    mongoose.Schema(
+      {
+        movieid: Number,
+        title: String,
+        published: Boolean,
+        released: Boolean,
+        poster_url: String,
+        release_date: String,
+        publish_date: String,
+        artists: [
+          {
+            artistid: Number,
+            first_name: String,
+            last_name: String,
+            wiki_url: String,
+            profile_url: String,
+            movies: [
+              {
+                type: String,
+              },
+            ],
           },
-          language: String,
-          show_timing: Date,
-          available_seats: String,
-          unit_price: Number,
-        },
-      ],
-    })
+        ],
+        genres: [
+          {
+            type: String,
+          },
+        ],
+        duration: Number,
+        critic_rating: { type: Number, min: 0, max: 5 },
+        trailer_url: String,
+        wiki_url: String,
+        story_line: String,
+        shows: [
+          {
+            id: Number,
+            theatre: { name: String, city: String },
+            language: String,
+            show_timing: String,
+            available_seats: String,
+            unit_price: Number,
+          },
+        ],
+      },
+      { timestamps: true }
+    )
   );
-
   return Movie;
 };

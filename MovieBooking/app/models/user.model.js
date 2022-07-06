5e0c03f6e618e153c5acd6cacfad58b79c0bfa3c
ShubhangiBane <98038587+ShubhangiBane@users.noreply.github.com>
2022-07-06 18:20:15 +0530

    Completed All Checkpoint

--- MovieBooking/app/models/user.model.js
+++ MovieBooking/app/models/user.model.js
@@ -1,20 +1,49 @@
+const validator = require('validator');
 module.exports = (mongoose) => {
   const User = mongoose.model(
     "user",
     mongoose.Schema(
       {
-        email: { type: String, unique: true, required: true, dropDups: true },
-        firstName: { type: String, required: true },
-        lastName: String,
-        username: { type: String, required: true },
+        userid: { type: Number, required: true },
+        email: {
+          type: String,
+          unique: true,
+          required: true,
+          lowercase: true,
+          validate: (value) => {
+            return validator.isEmail(value);
+          },
+        },
+        first_name: { type: String, required: true },
+        last_name: String,
+        username: String,
+        contact: { type: String, required: true },
         password: { type: String, required: true },
-        role: { type: String, default: "user" } /*types: admin ,user*/,
+        role: { type: String, default: "user", required: true },
         isLoggedIn: Boolean,
-        token: String,
+        uuid: String,
+        accesstoken: String,
+        coupens: [
+          {
+            id: Number,
+            discountValue: Number,
+          },
+        ],
+        bookingRequests: [
+          {
+            reference_number: Number,
+            coupon_code: Number,
+            show_id: Number,
+            tickets: [
+              {
+                type: Number,
+              },
+            ],
+          },
+        ],
       },
- 
+      { timestamps: true }
     )
   );
-
   return User;
 };
